f4b0d82b2dc319743fbe0c087b34f4f570dee187
msloekito <msloekito@gmail.com>
2016-01-06 16:39:57 -0800

    initial commit

---
+++ script/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+	// console.log("test");
+	// $('object').click(function () {
+ //  $(this).addClass('magictime puffIn');
+ //  console.log("aaa");
+// // })
+//  if ($(window).scrollTop() > 30) {
+//     $('object').addClass('magictime puffIn');
+//     $('object').css('display', 'block');
+//     console.log("ba");
+// 	}
+// 	else if ($(window).scrollTop() > 210) {
+// 	$('object').addClass('magictime puffOut');	
+// 	}
+
+});
+
